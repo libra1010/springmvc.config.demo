bd6740e7891c4564f429b75400c1f17535fc7de1
libra1010 <libra0907@outlook.com>
2017-01-11 16:57:49 +0800

    加入完整测试文件

---
+++ src/main/java/cn/libra/controller/HomeController.java
@@ -0,0 +1,36 @@
+package cn.libra.controller;
+
+import cn.libra.dao.IUserMapper;
+import cn.libra.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by clam on 2017/1/11.
+ */
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+
+    @Autowired
+    private IUserMapper userMapper = null;
+
+    @RequestMapping("")
+    public String index(){
+        return "index";
+    }
+
+
+    @RequestMapping(value="/get/{id}")
+    @ResponseBody
+    public User getUser(@PathVariable("id") Integer id){
+        User u =  userMapper.selectByPrimaryKey(1);
+        return u;
+    }
+}
